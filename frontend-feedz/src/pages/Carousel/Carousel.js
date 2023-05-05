d7c7c9ce7cf26b10708f093fdc8ed981c106c5c5
Jeferson Soares <52430346+jef-sorridente@users.noreply.github.com>
2023-05-05 19:59:11 -0300

    Carousel

--- frontend-feedz/src/pages/Carousel/Carousel.js
+++ frontend-feedz/src/pages/Carousel/Carousel.js
@@ -1,7 +1,22 @@
 import "./Carousel.js";
 
+import { Carousel, Slider } from "react-carousel"
+
 const Carousel = () => {
-  return <div>Carousel</div>;
+  const options = {
+    slidesToshow: 1,
+    transitionMs: 500
+  }
+  return <div>
+    {photos && photos.map((photo) => (
+      <Carousel key={photo._id}>
+        <Slide>
+          {photo.image && (<img src={`${uploads}/photos/${photo.image}`}
+            alt={photo.title}></img>)}
+        </Slide>
+      </Carousel>
+    ))}
+  </div>;
 };
 
 export default Carousel;
