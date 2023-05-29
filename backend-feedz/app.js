30100cba3104c2d1783eae4f226db766a1580269
Jeferson Soares <jpadilha53@gmail.com>
2023-05-29 18:14:51 -0300

    Teste http

--- backend-feedz/app.js
+++ backend-feedz/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
+const serverUrl = process.env.SERVER_URL;
 const port = process.env.PORT;
 
 const app = express();
--- backend-feedz/routes/Router.js
+++ backend-feedz/routes/Router.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express();
 
-router.use("/api/users", require("./UserRoutes"));
-router.use("/api/photos", require("./PhotoRoutes"));
+router.use("api/users", require("./UserRoutes"));
+router.use("api/photos", require("./PhotoRoutes"));
 
 //Teste de Rota
 router.get("/", (req, res) => {
